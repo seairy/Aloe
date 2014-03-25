c96a047f64135feb484a19263f06f96122bf9574
seairy <seairy@gmail.com>
2014-03-25 14:26:10 +0800

    init commit

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -2,7 +2,7 @@ class Mailer < ActionMailer::Base
   default from: "shihanxuehui@shihan.org.cn"
 
   def email email
-    mail(to: email.recipient, subject: email.subject){|format| format.html { render text: email.content }}
+    mail(to: email.recipient, subject: email.subject, content_type: 'text/html', body: email.content)
     email.sent!
   end
 end
